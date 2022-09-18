9648b18041a4485ab292a055a43cfd8b636b8161
sunil.kumar5062 <sunil.kumar5062@gmail.com>
2022-09-18 23:05:40 +0530

    Add project files.

---
+++ myTestProjectForPipeline/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+
+namespace myTestProjectForPipeline
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Set up framework");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass("Test is passed");
+        }
+    }
+}
